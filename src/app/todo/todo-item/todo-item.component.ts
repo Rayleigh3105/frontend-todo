cc0434fce0b4e187ed886ae356befb0a59e0df33
Moritz Vogt <moritz.vogt@vogges.de>
2018-08-13 17:11:53 +0200

    Added showing Todos

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoService} from '../todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  constructor( public $todo: TodoService ) {
+    $todo.getAllTodos().subscribe();
+  }
 
   ngOnInit() {
   }
